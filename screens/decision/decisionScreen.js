bece44021dc6aa18876c5d17d18f972fff8aed70
fengrenyuanhe0-collab <fengrenyuanhe0@gmail.com>
2026-04-02 20:25:05 +0500

    Add files via upload

--- screens/decision/decisionScreen.js
+++ screens/decision/decisionScreen.js
@@ -1,26 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
 
-export default function DecisionScreen() {
+const DecisionsScreen = ({ navigation }) => {
+  const handlePress = async () => {
+    try {
+      const people = await AsyncStorage.getItem("people");
+      const restaurants = await AsyncStorage.getItem("restaurants");
+
+      if (!people || JSON.parse(people).length === 0) {
+        Alert.alert(
+          "That ain't gonna work, chief",
+          "You haven't added any people. You should probably do that first, no?",
+          [{ text: "Ok" }],
+          { cancelable: false }
+        );
+      } else if (!restaurants || JSON.parse(restaurants).length === 0) {
+        Alert.alert(
+          "That ain't gonna work, chief",
+          "You haven't added any restaurants. You should probably do that first, no?",
+          [{ text: "Ok" }],
+          { cancelable: false }
+        );
+      } else {
+        navigation.navigate("WhosGoingScreen");
+      }
+    } catch (error) {
+      console.error("Failed to load data:", error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Decision Screen - Placeholder
-      </Text>
+    <View style={styles.decisionTimeScreenContainer}>
+      <TouchableOpacity
+        style={styles.decisionTimeScreenTouchable}
+        onPress={handlePress}
+      >
+        <Image source={require("../../assets/its-decision-time.android.png")} />
+        <Text style={{ paddingTop: 20 }}>Click the food to get going</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  decisionTimeScreenContainer: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+  decisionTimeScreenTouchable: {
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default DecisionsScreen;
